12d0a437ecd3b86010e1427a0d55f5e30ad629c2
michael.cardenas <michael.cardenas@realtechltda.com>
2025-08-28 12:14:10 -0500

    ss

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,11 +12,12 @@ class DashboardController extends Controller
    public function index()
 {
     $user = Auth::user();
-    
     if ($user->hasRole('admin')) {
-        return view('dashboard.admin');
+        $orders = Order::with(['items.product', 'user', 'country', 'city'])
+                      ->orderBy('created_at', 'desc')
+                      ->get();
+        return view('dashboard.admin', compact('orders'));
     }
-    
     // Si es comprador, buscamos sus órdenes con items y productos
     $orders = Order::where('user_id', $user->id)
                    ->with(['items.product', 'country', 'city']) // Cargar relaciones
